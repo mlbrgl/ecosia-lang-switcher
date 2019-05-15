9e85ade5ea85de5deebf3bc332b22495e706caaf
Matthieu Bergel <hello@matthieubergel.org>
2019-05-15 21:10:25 +0200

    fix: input filtering

--- src/components/RegionInput.vue
+++ src/components/RegionInput.vue
@@ -1,9 +1,9 @@
 <template>
   <input
     :value="value"
-    @:input="$emit('input', $event.target.value)"
     @keydown.up="$emit('up', $event.target.value)"
     @keydown.down="$emit('down', $event.target.value)"
+    @input="$emit('input', $event.target.value)"
   >
 </template>
 
