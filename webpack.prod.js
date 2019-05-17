c9d2d77b7413902ffe69a1b8ac49eb6e6ae6614f
Matthieu Bergel <hello@matthieubergel.org>
2019-05-17 15:28:30 +0200

    chore: add travis config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,32 +1,42 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
-const CopyPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const merge = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyPlugin(
       [
-        { from: "public/icons", to: "icons" },
+        { from: 'public/icons', to: 'icons' },
         {
-          from: "src/manifest.json",
-          transform: function(content, path) {
+          from: 'src/manifest.json',
+          transform(content, path) {
             // generates the manifest file using the package.json informations
             return Buffer.from(
               JSON.stringify({
                 description: process.env.npm_package_description,
                 name: process.env.npm_package_description,
                 version: process.env.npm_package_version,
-                ...JSON.parse(content.toString())
-              })
+                ...JSON.parse(content.toString()),
+              }),
             );
-          }
-        }
+          },
+        },
       ],
-      { copyUnmodified: true }
-    )
-  ]
+      { copyUnmodified: true },
+    ),
+  ],
 });
